c35a5bcdc7897bc0fd94528b85b838ae8041c6b8
LilWest03 <the.lil.west.1@gmail.com>
2026-04-26 12:54:26 +0700

    Export document model

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,6 +1,7 @@
 from app.models.base import Base
+from app.models.document import Document
 from app.models.task import Task
 from app.models.user import User
 from app.models.workspace import Workspace
 
-__all__ = ["Base", "User", "Workspace", "Task"]
+__all__ = ["Base", "User", "Workspace", "Task", "Document"]
